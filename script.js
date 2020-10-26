667b3d972218c3ebf12a8a2da5b7bb3bcbf9a3c2
ranay4626 <73483198+ranay4626@users.noreply.github.com>
2020-10-26 13:43:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Rana Y"
+let userName = "Ranay4626";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
